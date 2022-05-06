9e3d83a86d5c8faad732fd2a82dfdd56bb5a3384
rahulkumar1907 <rkrahulkv143@gmail.com>
2022-05-06 14:33:58 +0530

    final submission

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,7 +4,7 @@ const collegeController = require("../controllers/collegeController")
  const internController = require("../controllers/internController")
 
 
-router.post("functionup/colleges",collegeController.createCollege)
+router.post("/functionup/colleges",collegeController.createCollege)
 
  router.post("/functionup/interns",internController.createIntern)
 
